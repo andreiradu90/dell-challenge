3b78f5c5e41e6fd9f953d5a52a80d8ac7898605c
Andrei Radu <andrei.radu90@ymail.com>
2019-12-14 17:18:09 +0200

    commit B

--- DellChallenge/DellChallenge.B/Program.cs
+++ DellChallenge/DellChallenge.B/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DellChallenge.B
 {
@@ -9,54 +11,113 @@ namespace DellChallenge.B
             // Given the classes and interface below, please constructor the proper hierarchy.
             // Feel free to refactor and restructure the classes/interface below.
             // (Hint: Not all species and Fly and/or Swim)
+            (new List<Species>
+            {
+                new Human(),
+                new Bird(),
+                new Fish()
+            }).ForEach(c => c.GetSpecies());
+
+            (new List<ITerrestrialSpecies> //similar for other species
+            {
+                new Human() // other ITerrestrialSpecies species could be added which have a different implementation of Eat(), Drink(), Swim()
+            }).ForEach(c =>
+            {
+                c.Eat();
+                c.Drink();
+                c.Swim();
+            });
         }
     }
 
-    public interface ISpecies
+    //It's best that interfaces are segregated
+    public interface ITerrestrialSpecies
+    {
+        void Eat();
+        void Drink();
+        void Swim();
+    }
+    public interface IAquaticSpecies
+    {
+        void Eat();
+        void Swim();
+    }
+    public interface IAerialSpecies
     {
         void Eat();
         void Drink();
         void Fly();
-        void Swim();
     }
-
-    public class Species
+    public abstract class Species
     {
+        // We can keep this as a default implementation
         public virtual void GetSpecies()
         {
             Console.WriteLine($"Echo who am I?");
         }
     }
 
-    public class Human : ISpecies
+    public class Human : Species, ITerrestrialSpecies
     {
+        public override void GetSpecies()
+        {
+            Console.WriteLine($"I am a human!");
+        }
         public void Drink()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"A human can drink!");
         }
 
         public void Eat()
         {
-            throw new NotImplementedException();
-        }
-
-        public void Fly()
-        {
-            throw new NotImplementedException();
+            Console.WriteLine($"A human can eat!");
         }
 
         public void Swim()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"A human can usually swim!");
         }
     }
 
-    public class Bird
+    public class Bird : Species, IAerialSpecies
     {
+        public override void GetSpecies()
+        {
+            Console.WriteLine($"I am a bird!");
+        }
+
+        public void Drink()
+        {
+            Console.WriteLine($"A bird can drink!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"A bird can eat!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"A bird can fly!");
+        }
     }
 
-    public class Fish
+    public class Fish : Species, IAquaticSpecies
     {
+        public override void GetSpecies()
+        {
+            Console.WriteLine($"I am a fish!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"A fish can eat!");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine($"A fish can swim!");
+        }
     }
 }
 
